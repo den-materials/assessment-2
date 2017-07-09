141968eb4d11a153b4327827d679e63b67728153
Mark <MarkKleinfelder@gmail.com>
2017-07-08 21:32:20 -0600

    adds js

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 window.onload = function ready(){
 	console.log('Ready');
+	
 };
 
 
@@ -11,35 +12,39 @@ var bluePos = 0;
 var blueCounter = 0;
 var redCounter = 0;
 
-if(blueCounter == 10){
-      	document.removeEventListener("keydown", Blue, false);
-      	console.log('Blue is the winner!');
-      }
 
 
 document.addEventListener("keypress", function Red(event) {
+	
+  if(redCounter == 10 || blueCounter == 10){
+  document.removeEventListener("keypress", Red, false);
+  console.log('Red is the winner!');
+       };
   if(event.keyCode == 122){
-	redCounter += 1;
-	redPos += 25;
+  	redCounter += 1;
+  	redPos += 25;
 	redBox.style.left = redPos + "px";
 	console.log('pressed z');
 	console.log(redCounter);
+	
 	}
-	  if(redCounter == 10){
-	  	console.log('Red is the winner!')
-	  }
 });
 
 document.addEventListener("keydown", function Blue(event) {
-  if(event.keyCode == 39){
-	blueCounter += 1;
-    bluePos += 25;
-	blueBox.style.left = bluePos + "px";
-	console.log('pressed right');
-	console.log(blueCounter);
+    ;
+  if(blueCounter == 10 || redCounter == 10){
+  	document.removeEventListener("keydown", Blue, false);
+	console.log('Blue is the winner!');
+     };
+   if(event.keyCode === 39){
+   	blueCounter += 1
+	  bluePos += 25;
+	  blueBox.style.left = bluePos + "px";
+	  console.log('pressed right');
+	  console.log(blueCounter);
+	  
     }
-      
-});
+ });
 
 // key z = 122 or 90?
 // 32 keys to win @ 25px
